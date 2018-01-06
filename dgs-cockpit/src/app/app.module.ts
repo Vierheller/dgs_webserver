d222781c68a98aee24b9346299e5f65a4b3e47aa
gabkol <gabkol@web.de>
2018-01-06 18:18:43 +0100

    add sections and structure

--- dgs-cockpit/src/app/app.module.ts
+++ dgs-cockpit/src/app/app.module.ts
@@ -1,22 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material';
-
+import { MatTabsModule } from '@angular/material/tabs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { PictureComponent } from './dashboard/picture/picture.component';
+import { TelemetryComponent } from './dashboard/telemetry/telemetry.component';
+import { TimelineComponent } from './dashboard/timeline/timeline.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ChartComponent } from './history/chart/chart.component';
+import { SelectionComponent } from './history/selection/selection.component';
+import { HistoryComponent } from './history/history.component';
+import { LogComponent } from './log/log.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    PictureComponent,
+    TelemetryComponent,
+    TimelineComponent,
+    DashboardComponent,
+    ChartComponent,
+    SelectionComponent,
+    HistoryComponent,
+    LogComponent,
   ],
   imports: [
     BrowserModule,
     MatButtonModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
